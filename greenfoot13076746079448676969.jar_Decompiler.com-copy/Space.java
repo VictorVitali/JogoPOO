64a6fd1092ca3d8e7f16ba832be9f9d9b15839f0
Victor Vitali <victorvitali@gmail.com>
2026-05-27 17:00:41 -0300

    add respawm inimigos

--- greenfoot13076746079448676969.jar_Decompiler.com-copy/Space.java
+++ greenfoot13076746079448676969.jar_Decompiler.com-copy/Space.java
@@ -5,6 +5,7 @@ import greenfoot.World;
 public class Space extends World {
    public static int PONTOS = 0;
    int sec = 0;
+   int tempoRespawn = 0;
 
    public Space() {
       super(200, 600, 1);
@@ -15,7 +16,10 @@ public class Space extends World {
    private void prepare() {
       Nave1 n1 = new Nave1();
       this.addObject(n1, 151, 558);
+      this.criarInimigos();
+   }
 
+   private void criarInimigos() {
       int[] posicoesX = {30, 65, 100, 135, 170};
       for (int i = 0; i < posicoesX.length; i++) {
          this.addObject(new Inimigo(), posicoesX[i], 40);
@@ -28,6 +32,16 @@ public class Space extends World {
       this.getBackground().drawImage(bg, 0, scrollAmt);
       this.getBackground().drawImage(bg, 0, scrollAmt - this.getHeight());
       this.showText("Pontos: " + PONTOS, 120, 30);
+
+      if (this.getObjects(Inimigo.class).isEmpty()) {
+         this.tempoRespawn++;
+         if (this.tempoRespawn >= 120) {
+            this.criarInimigos();
+            this.tempoRespawn = 0;
+         }
+      } else {
+         this.tempoRespawn = 0;
+      }
 
    }
    
